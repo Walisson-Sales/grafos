09da4c196d643c9b7875b513359f9ae253c4b787
Walisson-Sales <walissonestudando@gmail.com>
2026-05-25 10:58:00 -0300

    adicionando dependência do Lombok

--- pratica/grafos-melhorado/build.gradle.kts
+++ pratica/grafos-melhorado/build.gradle.kts
@@ -10,6 +10,10 @@ repositories {
 }
 
 dependencies {
+    val lombokVersion = "1.18.38"
+    compileOnly("org.projectlombok:lombok:$lombokVersion")
+    annotationProcessor("org.projectlombok:lombok:$lombokVersion")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
